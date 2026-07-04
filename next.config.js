a10f4c1d2fe203d281569b125742c063ccbf592b
[󰽥🌿] y3hll0what_ <146971455+Herbert-byte@users.noreply.github.com>
2026-07-04 19:37:03 +0530

    Adding requirements for deploying Github Pages.
    Added all the requirements such as -
    *  next.config.js has basePath/assetPrefix set for production, and npm
    run export is added to package.json.

--- next.config.js
+++ next.config.js
@@ -1,1 +1,6 @@
-module.exports = {};
+const isProd = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  basePath: isProd ? '/portfolio' : '',
+  assetPrefix: isProd ? '/portfolio/' : '',
+};
