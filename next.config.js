6747bc7ba79869268ebbfe1279f58fb195cd9cc4
[󰽥🌿] y3hll0what_ <146971455+Herbert-byte@users.noreply.github.com>
2026-07-04 19:47:59 +0530

    Fixed deployment error

--- next.config.js
+++ next.config.js
@@ -1,6 +1,1 @@
-const isProd = process.env.NODE_ENV === 'production';
-
-module.exports = {
-  basePath: isProd ? '/portfolio' : '',
-  assetPrefix: isProd ? '/portfolio/' : '',
-};
+module.exports = {};
