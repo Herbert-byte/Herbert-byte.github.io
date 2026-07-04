82ab4c6271644d1900a5977032015d72daac2678
[󰽥🌿] y3hll0what_ <146971455+Herbert-byte@users.noreply.github.com>
2026-07-04 20:51:47 +0530

    fix: replace Nerd Font v2 codepoints with v3-compatible Font Awesome icons

--- src/utils/bin/sumfetch.ts
+++ src/utils/bin/sumfetch.ts
@@ -6,12 +6,12 @@ const sumfetch = async (args: string[]): Promise<string> => {
                                                         -----------
                   y__                                    ABOUT
                   ~R@@@g_                                ${config.name}
-                    ~@@@@@g,                            ﰩ ${config.ps1_hostname}
-        yg@@@:       \`4@@@@F                             <u><a href="${config.resume_url}" target="_blank">resume</a></u>
-        \`@@@@_.      ::g@@@                             爵 <u><a href="${config.repo}" target="_blank">Github repo</a></u>
+                    ~@@@@@g,                             ${config.ps1_hostname}
+        yg@@@:       \`4@@@@F                             <u><a href="${config.resume_url}" target="_blank">resume</a></u>
+        \`@@@@_.      ::g@@@                              <u><a href="${config.repo}" target="_blank">Github repo</a></u>
           ~@@@@gy__::y@@@@                              -----------
-           \`~@@@@@@@\$@@@@@y                              CONTACT
-              \`~4@@@@@@@@@@@,                            <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
+           \`~@@@@@@@\$@@@@@y                              CONTACT
+              \`~4@@@@@@@@@@@,                            <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
                   \`~~4@@@@@@@                            <u><a href="https://github.com/${config.social.github}" target="_blank">github.com/${config.social.github}</a></u>
                         \`\`~~                        
                                                     
