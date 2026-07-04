a79ea920f6d0c6e33e101359cd11b61ecbabf1fe
[󰽥🌿] y3hll0what_ <146971455+Herbert-byte@users.noreply.github.com>
2026-07-04 20:31:01 +0530

    fix: correct Nerd Font icons in sumfetch, add font format hint

--- src/utils/bin/sumfetch.ts
+++ src/utils/bin/sumfetch.ts
@@ -2,21 +2,21 @@ import config from '../../../config.json';
 
 const sumfetch = async (args: string[]): Promise<string> => {
   return `
-                                                \uf08b sumfetch: summary display
-                                                -----------
-              y__                               \u2652 ABOUT
-              ~R@@@g_                           \uf515 ${config.name}
-                ~@@@@@g,                        \uf030 ${config.ps1_hostname}
-    yg@@@:       \`4@@@@F                        \uf5e7 <u><a href="${config.resume_url}" target="_blank">resume</a></u>
-    \`@@@@_.      ::g@@@                         \ufa9e <u><a href="${config.repo}" target="_blank">Github repo</a></u>
-      ~@@@@gy__::y@@@@                          -----------
-       \`~@@@@@@@\$@@@@@y                         \u2021 CONTACT
-          \`~4@@@@@@@@@@@,                       \u1eef <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
-              \`~~4@@@@@@@                       \uf0a2 <u><a href="https://github.com/${config.social.github}" target="_blank">github.com/${config.social.github}</a></u>
-                    \`\`~~                    
-                                            
-                                            
-                                            
+                                                         sumfetch: summary display
+                                                        -----------
+                  y__                                    ABOUT
+                  ~R@@@g_                                ${config.name}
+                    ~@@@@@g,                            ﰩ ${config.ps1_hostname}
+        yg@@@:       \`4@@@@F                             <u><a href="${config.resume_url}" target="_blank">resume</a></u>
+        \`@@@@_.      ::g@@@                             爵 <u><a href="${config.repo}" target="_blank">Github repo</a></u>
+          ~@@@@gy__::y@@@@                              -----------
+           \`~@@@@@@@\$@@@@@y                              CONTACT
+              \`~4@@@@@@@@@@@,                            <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
+                  \`~~4@@@@@@@                            <u><a href="https://github.com/${config.social.github}" target="_blank">github.com/${config.social.github}</a></u>
+                        \`\`~~                        
+                                                    
+                                                    
+                                                    
 `;
 };
 
